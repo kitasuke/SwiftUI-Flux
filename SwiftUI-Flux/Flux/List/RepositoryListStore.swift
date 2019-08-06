63d00f57599f7e51ae233d92d0bd2eed40a8dc80
kitasuke <yusuke2759@gmail.com>
2019-08-06 18:48:00 +0900

    Use @Published

--- SwiftUI-Flux/Flux/List/RepositoryListStore.swift
+++ SwiftUI-Flux/Flux/List/RepositoryListStore.swift
@@ -12,23 +12,13 @@ import SwiftUI
 
 final class RepositoryListStore: ObservableObject {
     static let shared = RepositoryListStore()
-    let objectWillChange: AnyPublisher<Void, Never>
-    private let objectWillChangeSubject = PassthroughSubject<Void, Never>()
     
-    private(set) var repositories: [Repository] = [] {
-        didSet { objectWillChangeSubject.send(()) }
-    }
-    var isErrorShown = false {
-        didSet { objectWillChangeSubject.send(()) }
-    }
-    var errorMessage = ""
-    private(set) var shouldShowIcon = false {
-        didSet { objectWillChangeSubject.send(()) }
-    }
+    @Published private(set) var repositories: [Repository] = []
+    @Published var isErrorShown = false
+    @Published var errorMessage = ""
+    @Published private(set) var shouldShowIcon = false
     
     init(dispatcher: RepositoryListDispatcher = .shared) {
-        objectWillChange = objectWillChangeSubject.eraseToAnyPublisher()
-        
         dispatcher.register { [weak self] (action) in
             guard let strongSelf = self else { return }
             
